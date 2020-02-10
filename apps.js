d95c40a10d64cea052a3c04786c51759d896fa5b
robbymo81 <robert.l.mosher@gmail.com>
2020-02-10 15:34:01 -0800

    cookies number

---
+++ apps.js
@@ -0,0 +1,63 @@
+//Conditions for the test
+// coookies to make depends on hours of operations (6pm to 8pm) all locations
+//each location will have minimum number of customers per hour
+//maximum number of customers per hour
+//average number of cookies purchased per customer
+
+//application needs to be easily modified
+
+//this will be the store hours
+
+var hours = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', 
+             '12pm:', '1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm:', '7pm:'];
+
+var location = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
+
+function getRandomInt (max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+getRandomInt(800)
+var storeLocation {
+  location: 'cookies',
+  custumer : 'numberOfCustomers',
+  cookies : 'numberOfCookies',
+  hourOfOperations : 'hour',
+  largeCustomer : 'maximumNumberOfCustomer',
+  smallCustomer : 'minimumNumberOfCustomer',
+  cookiesSold : 'averageCookiesSold',
+}
+
+function randomCookies (){
+  let customerNum = 
+}
+
+// for each day create a new branch of the lab using the following naming conventions class##-feature
+
+
+
+//1 store min/max hourly and average cookies per customer in object properties
+
+//2 method of store object to generate a random number of customers per hour use random math generator.
+
+//3 calculate and store simulated amounts of cookies purchased for each hour per location using cookies purchase and the random number of customers
+
+//4 store the results for each location in a seperate array make as a property of the object representing that location
+
+
+//Display the values of each array as unordered list in browser
+
+//Claculating the sum of these hourly totals output for each location 
+
+//
+
+// locations and time
+
+//string numbers for objects
+
+//shop locatiions
+
+
+
+//simulated amount of cookies
+
+//
